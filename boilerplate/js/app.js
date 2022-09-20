93754e082643eb81eee15a96b4d03b1ab7f8551b
lanbow93 <the.doctor1501@gmail.com>
2022-09-20 18:12:21 -0400

    Function to test app.js functionality

--- boilerplate/js/app.js
+++ boilerplate/js/app.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+
+//Function to alternate the title between red and purple when clicked
+function beenClicked() {
+  let titleElement = document.querySelector("h1");
+  let test = getComputedStyle(titleElement).getPropertyValue("color");
+
+  if (test === "rgb(138, 43, 226)") {
+    document.querySelector("h1").setAttribute("style", "color: red");
+  } else {
+    document.querySelector("h1").setAttribute("style", "color: blueviolet");
+  }
+}
+
+document.querySelector("h1").addEventListener("click", beenClicked);
